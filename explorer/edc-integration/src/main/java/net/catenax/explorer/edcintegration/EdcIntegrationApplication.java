608520de2903a78d7766bddd35131dd6856cca7e
maciejkizlich-zf <maciejkizlich@MacBook-Pro-Maciej.local>
2022-05-26 10:30:45 +0200

    Initial commit

---
+++ explorer/edc-integration/src/main/java/net/catenax/explorer/edcintegration/EdcIntegrationApplication.java
@@ -0,0 +1,13 @@
+package net.catenax.explorer.edcintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdcIntegrationApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(EdcIntegrationApplication.class, args);
+  }
+
+}
