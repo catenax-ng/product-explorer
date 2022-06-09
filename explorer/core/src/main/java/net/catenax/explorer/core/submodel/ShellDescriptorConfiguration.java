ff9b330f37eefe9fa1bcaea57264b45133068ebc
Szajnowski <leszek.szajnowksi.external@zf.com>
2022-06-09 09:52:36 +0200

    twin registry integration - add README

--- explorer/core/src/main/java/net/catenax/explorer/core/submodel/ShellDescriptorConfiguration.java
+++ explorer/core/src/main/java/net/catenax/explorer/core/submodel/ShellDescriptorConfiguration.java
@@ -3,15 +3,17 @@ package net.catenax.explorer.core.submodel;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import net.catenax.explorer.core.submodel.twinregistry.TwinRegistryAssetProvider;
 import net.catenax.explorer.core.submodel.twinregistry.TwinRegistryClient;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
-public class SubmodelConfiguration {
+public class ShellDescriptorConfiguration {
 
   @Bean
-  ShellDescriptorProvider submodelProvider(WebClient webClient, ObjectMapper mapper) {
+  @ConditionalOnProperty(value = "edc.shell-provider", havingValue = "twin-registry", matchIfMissing = true)
+  ShellDescriptorProvider shellDescriptorProvider(WebClient webClient, ObjectMapper mapper) {
     return new TwinRegistryAssetProvider(
         new TwinRegistryClient(webClient, mapper)
     );
