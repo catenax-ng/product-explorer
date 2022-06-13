ebe90ddc407b01e3b41b399e60b3744f793f878d
Kamil Toszek <kamil.toszek@gmail.com>
2022-06-13 16:28:03 +0200

    added web-ui controller to core module mock query response from custom query component

--- explorer/core/src/main/java/net/catenax/explorer/core/ExplorerConfiguration.java
+++ explorer/core/src/main/java/net/catenax/explorer/core/ExplorerConfiguration.java
@@ -1,19 +1,26 @@
 package net.catenax.explorer.core;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.catenax.explorer.core.edclocation.EdcLocationProvider;
-import net.catenax.explorer.core.retriever.AssetRetriever;
 import net.catenax.explorer.core.submodel.ShellDescriptorProvider;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @Slf4j
+@RequiredArgsConstructor
 public class ExplorerConfiguration {
-
   @Bean
   ExplorerController explorerController(ExplorerService service) {
     return new ExplorerController(service);
+  }
+
+  @Bean
+  ExplorerWebController explorerWebController(@Value("${explorer-application-query-url:http://localhost:8080/v1/assets/mockup/}") String explorerApplicationUrl, ObjectMapper objectMapper) {
+    return new ExplorerWebController(explorerApplicationUrl, objectMapper);
   }
 
   @Bean
