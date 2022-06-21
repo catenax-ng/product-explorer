57482deecdb2b56d14ef5cf2279ec5ffb9d76c6e
maciejkizlich-zf <maciejkizlich@MacBook-Pro-Maciej.local>
2022-06-21 14:17:00 +0200

    Improve mocked SDH response - improve naming strategy

--- self-description-hub-integration/src/main/java/net/catenax/explorer/selfdescriptionhub/web/SelfDescription.java
+++ self-description-hub-integration/src/main/java/net/catenax/explorer/selfdescriptionhub/web/SelfDescription.java
@@ -1,5 +1,7 @@
 package net.catenax.explorer.selfdescriptionhub.web;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,6 +11,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class SelfDescription {
 
   String companyNumber;
@@ -23,3 +26,4 @@ public class SelfDescription {
 
   String type;
 }
+
