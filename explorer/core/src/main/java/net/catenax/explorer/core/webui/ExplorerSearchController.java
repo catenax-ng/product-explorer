386592c5aad46f8563f5cd4721f6f24697f7efae
Kamil Toszek <kamil.toszek@gmail.com>
2022-06-27 14:43:19 +0200

    added catena-x logo added row click which displays item json payload

--- explorer/core/src/main/java/net/catenax/explorer/core/webui/ExplorerSearchController.java
+++ explorer/core/src/main/java/net/catenax/explorer/core/webui/ExplorerSearchController.java
@@ -1,7 +1,10 @@
 package net.catenax.explorer.core.webui;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import net.catenax.explorer.core.webui.dto.SearchResultDto;
 import net.catenax.explorer.core.webui.service.SearchResultsProvider;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,11 +12,15 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RequestMapping("/")
 @RequiredArgsConstructor
 @Slf4j
 public class ExplorerSearchController {
     private final SearchResultsProvider searchResultsProvider;
+    private final ObjectMapper objectMapper;
 
     @GetMapping()
     public String index(Model model) {
@@ -22,7 +29,15 @@ public class ExplorerSearchController {
 
     @PostMapping("/ui/search")
     public String search(@RequestParam("query") String query, Model model) {
-        model.addAttribute("results", searchResultsProvider.search(query));
+        List<SearchResultDto> searchResults = new ArrayList<>();
+        searchResultsProvider.search(query).forEach(descriptor -> {
+            try {
+                searchResults.add(new SearchResultDto(descriptor, objectMapper.writeValueAsString(descriptor)));
+            } catch (JsonProcessingException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        model.addAttribute("results", searchResults);
         return "search-page-results";
     }
 }
