cf168f155baa7cf05e88ab4991bf81d0252f1d9b
Kamil Toszek <kamil.toszek@gmail.com>
2022-06-29 14:03:29 +0200

    backend-data-service added to docker-compose

---
+++ backend-data-service/src/main/java/net/catenax/explorer/data/BackendDataServiceApplication.java
@@ -0,0 +1,13 @@
+package net.catenax.explorer.selfdescriptionhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendDataServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BackendDataServiceApplication.class, args);
+  }
+
+}
