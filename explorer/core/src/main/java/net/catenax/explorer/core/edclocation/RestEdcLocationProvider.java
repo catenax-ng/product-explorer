c58a5840aa304d5391b2fd758b2ba85a194ee38a
maciejkizlich-zf <maciejkizlich@MacBook-Pro-Maciej.local>
2022-08-11 14:02:45 +0200

    Do not throw 500 errors when SDH does not answer correctly

--- explorer/core/src/main/java/net/catenax/explorer/core/edclocation/RestEdcLocationProvider.java
+++ explorer/core/src/main/java/net/catenax/explorer/core/edclocation/RestEdcLocationProvider.java
@@ -2,13 +2,16 @@ package net.catenax.explorer.core.edclocation;
 
 import static java.time.Duration.ofSeconds;
 
+import java.util.ArrayList;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.catenax.explorer.core.edclocation.model.SelfDescription;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 @RequiredArgsConstructor
 @Slf4j
@@ -29,7 +32,13 @@ public class RestEdcLocationProvider implements EdcLocationProvider {
             .path(selfDescriptionHubUrl)
             .build())
         .retrieve()
+        .onStatus(HttpStatus::is5xxServerError, response -> {
+          log.error("Could not obtain known edc locations: " + response.statusCode());
+          return Mono.empty();
+        })
         .bodyToMono(LIST_SELF_DESCRIPTION_TYPE_REFERENCE)
+        .onErrorReturn(new ArrayList<>())
         .block(ofSeconds(10));
   }
+
 }
