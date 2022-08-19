0444a29c62d80517eb2dab32909091e262ae4a1a
leszekszajnowski-zf <105491433+leszekszajnowski-zf@users.noreply.github.com>
2022-08-19 10:23:35 +0200

    rearrange the project, add readme and make some improvment in the doc… (#34)
    * rearrange the project, add readme and make some improvment in the docker-compose.yaml
    
    * delete comments

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o errtrace
+set -o pipefail
+set -o nounset
+
+cd self-description-hub
+mvn clean package -DskipTests
+docker image build -t self-description-hub .
+
+cd ../explorer/edc-integration/edc
+./gradlew publishToMavenLocal -x test
+
+cd ../edc-controlplane-memory
+./gradlew clean build
+
+cd ../edc-dataplane
+./gradlew clean build
