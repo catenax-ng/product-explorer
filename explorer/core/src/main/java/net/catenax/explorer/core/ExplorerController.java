2aa091b4e37c4bed763e20f2999102c9e0766622
Kamil Toszek <kamil.toszek@gmail.com>
2022-09-14 19:29:21 +0200

    fix: Wrong annotation on ExplorerController search endpoint

--- explorer/core/src/main/java/net/catenax/explorer/core/ExplorerController.java
+++ explorer/core/src/main/java/net/catenax/explorer/core/ExplorerController.java
@@ -5,8 +5,8 @@ import lombok.extern.slf4j.Slf4j;
 import net.catenax.explorer.core.shell.ShellDescriptorResponse.ShellDescriptor;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import reactor.core.publisher.Flux;
 
@@ -18,7 +18,7 @@ public class ExplorerController {
   final ExplorerService explorerService;
 
   @PostMapping(path = "/stream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-  public @ResponseBody Flux<ShellDescriptor> search(@RequestParam final QueryCommand command) {
+  public @ResponseBody Flux<ShellDescriptor> search(@RequestBody final QueryCommand command) {
     log.info("Querying for Asset by query: " + command);
     return explorerService.search(command);
   }
